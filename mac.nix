42d77621841f656a3f3841c89689606c68907343
godelized <44802527+godelized@users.noreply.github.com>
2022-08-14 19:09:15 +0200

    Add macOS releases

---
+++ mac.nix
@@ -0,0 +1,71 @@
+{
+  x64 = {
+    prod = {
+      channel = "prod";
+      hash = "sha512-OkpsWKT1/RTkhMq3sWylfNEX0YKudVbpssOzVf8Dy63XqhuP+7rFO1Js4My9CdHZh3DKLhle1KffQQKqSG2Ggg==";
+      name = "shadow-client";
+      releaseDate = "2022-08-04T11:58:49.804Z";
+      subSystem = "x64";
+      system = "mac";
+      url = "https://update.shadow.tech/launcher/prod/mac/x64/Shadow-5.0.1155.zip";
+      version = "5.0.1155";
+    };
+
+    preprod = {
+      channel = "preprod";
+      hash = "sha512-iHhyfI5hrjGKICbbB0NeVc0IsOBvMkvowoNoUb1nlnlEkKcD3f9A1SfAVSEDrfudvsxVLEg9WBAtNTqvQRCwFw==";
+      name = "shadow-client-preprod";
+      releaseDate = "2022-08-02T12:35:51.216Z";
+      subSystem = "x64";
+      system = "mac";
+      url = "https://update.shadow.tech/launcher/preprod/mac/x64/ShadowBeta-5.0.1153.zip";
+      version = "5.0.1153";
+    };
+
+    testing = {
+      channel = "testing";
+      hash = "sha512-dy0KCnzLb6NKeYO03vafpP0gAyGG4boFR0RFbwj1aE61ExC6h55r7nWPHG1Wkq7FcG4H2B5dsUdlhJP2jcL1PQ==";
+      name = "shadow-client-testing";
+      releaseDate = "2022-08-02T12:19:46.512Z";
+      subSystem = "x64";
+      system = "mac";
+      url = "https://update.shadow.tech/launcher/testing/mac/x64/ShadowAlpha-5.0.1152.zip";
+      version = "5.0.1152";
+    };
+  };
+
+  arm64 = {
+    prod = {
+      channel = "prod";
+      hash = "sha512-RFdDEq97U+IPLrzw1zhNAuj56wzsbmlKiJavzx2v7BlJAXLHqxlBIDMgKnxR4y2KYMWgawr4ryIZ20R1OPVafg==";
+      name = "shadow-client";
+      releaseDate = "2022-08-04T11:54:52.485Z";
+      subSystem = "arm64";
+      system = "mac";
+      url = "https://update.shadow.tech/launcher/prod/mac/arm64/Shadow-5.0.1155.zip";
+      version = "5.0.1155";
+    };
+
+    preprod = {
+      channel = "preprod";
+      hash = "sha512-3LcztmjpWxr6e6QLZN+Rx8jCx1rzjezJ/+0g+CS6lGtbFnEPtL45/8+QT2JAgP/d12CrwupqkYqrEfB9anyJfQ==";
+      name = "shadow-client-preprod";
+      releaseDate = "2022-08-02T12:36:24.743Z";
+      subSystem = "arm64";
+      system = "mac";
+      url = "https://update.shadow.tech/launcher/preprod/mac/arm64/ShadowBeta-5.0.1153.zip";
+      version = "5.0.1153";
+    };
+
+    testing = {
+      channel = "testing";
+      hash = "sha512-4X1b8PXsA34QcagAGJK6uNoIaQyK9B6JF5JjGMcLxSXMUeepMnjluApKLyIuIqvEACnku9dV1Rw2FAva6iM7Zg==";
+      name = "shadow-client-testing";
+      releaseDate = "2022-08-02T12:20:32.231Z";
+      subSystem = "arm64";
+      system = "mac";
+      url = "https://update.shadow.tech/launcher/testing/mac/arm64/ShadowAlpha-5.0.1152.zip";
+      version = "5.0.1152";
+    };
+  };
+}
