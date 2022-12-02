b14e92e2085d9f6a213cb0513f5298358f4bd149
godelized <44802527+godelized@users.noreply.github.com>
2022-12-02 19:33:41 +0100

    Update linux releases

--- linux.nix
+++ linux.nix
@@ -2,35 +2,35 @@
   ubuntu_18-04 = {
     prod = {
       channel = "prod";
-      hash = "sha512-tdAqs+ShXbxnjVn8bIQD4PlKL++2MDyvkhPgIuaUFc+z7yqDcNhL7RkXnCSQYa/rkd/a2O9GxX4BmM3sIBnaSQ==";
+      hash = "sha512-YT/KaiAt4ZCFGiOTR4wxEEDG4mtskhbH4Gks5iTomrc2ajzhWBScVio6KnDYdOyLo1X0JAZbQjBa84w8M1rkUA==";
       name = "shadow-client";
-      releaseDate = "2022-09-08T09:40:16.941Z";
+      releaseDate = "2022-11-07T17:32:07.591Z";
       subSystem = "ubuntu_18.04";
       system = "linux";
-      url = "https://update.shadow.tech/launcher/prod/linux/ubuntu_18.04/Shadow-5.0.1168.AppImage";
-      version = "5.0.1168";
+      url = "https://update.shadow.tech/launcher/prod/linux/ubuntu_18.04/Shadow-8.0.10018.AppImage";
+      version = "8.0.10018";
     };
 
     preprod = {
       channel = "preprod";
-      hash = "sha512-C8q6q+k37WQ40eRUnEuPYDAYbbnaq9x0GoLF6iRA/Z9ZaXBLrLTd++FU2u+QSnaeUnIy4nslO9SY4glohySIuw==";
+      hash = "sha512-/HMTpcHUBsWTP9dB3F1KGNlT0+mhfaYqmeHqhJ4JGd2Q7ITU5iVNGE0C0crZG+8m92u2lSNRhwSN0zUv/5BXxw==";
       name = "shadow-client-preprod";
-      releaseDate = "2022-09-26T09:38:42.145Z";
+      releaseDate = "2022-10-26T14:26:56.807Z";
       subSystem = "ubuntu_18.04";
       system = "linux";
-      url = "https://update.shadow.tech/launcher/preprod/linux/ubuntu_18.04/ShadowBeta-5.0.1182.AppImage";
-      version = "5.0.1182";
+      url = "https://update.shadow.tech/launcher/preprod/linux/ubuntu_18.04/ShadowBeta-8.0.10012.AppImage";
+      version = "8.0.10012";
     };
 
     testing = {
       channel = "testing";
-      hash = "sha512-ZKkRx7//ClzNDsSVGiML+lYQeqFNwi/XH6+bHMYxSLEeuN52qGlA3FMk++fZHfDNuY78Ko/GS7S/xFt+gzUgOQ==";
+      hash = "sha512-loxi352FncQWiOX7unWLHgFHOXSdYdXMsUwEmxRwvw7jiprjNSjzXitU9zGMQHO22KcI2diKCVA8GbIjFRd4hw==";
       name = "shadow-client-testing";
-      releaseDate = "2022-10-11T07:45:47.846Z";
+      releaseDate = "2022-11-30T10:38:54.092Z";
       subSystem = "ubuntu_18.04";
       system = "linux";
-      url = "https://update.shadow.tech/launcher/testing/linux/ubuntu_18.04/ShadowAlpha-8.0.10002.AppImage";
-      version = "8.0.10002";
+      url = "https://update.shadow.tech/launcher/testing/linux/ubuntu_18.04/ShadowAlpha-8.0.10029.AppImage";
+      version = "8.0.10029";
     };
   };
 }
